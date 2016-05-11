70b87dec0c635f77e29be19d99edf6a3b6412e58
Steve Troetti <workstroetti@gmail.com>
2016-05-11 12:34:09 -0400

    Attempt to fix broken openshift build

--- post-install.sh
+++ post-install.sh
@@ -5,6 +5,13 @@ set -o errexit
 bower=$(npm bin)'/bower'
 gulp=$(npm bin)'/gulp'
 
+if [ -z $OPENSHIFT_REPO_DIR ]; then
+    echo 'not on openshift'
+else
+    echo 'on openshift'
+    HOME=$HOME/app-root/runtime
+fi
+
 echo 'post install script running...'
 echo 'running bower...'
 $bower install
