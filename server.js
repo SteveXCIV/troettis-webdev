b7d8e3c8fe7c782b6415f6ea3768c39b64a265e9
Steve Troetti <workstroetti@gmail.com>
2016-06-05 13:33:12 -0400

    Added global UUID generator function to app

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+var uuid = require('node-uuid');
+// Attach a global ID generating function to app
+app.getNextId = function() {
+    return uuid.v4();
+}
+
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
