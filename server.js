3d5cc946f50c0fb280a73ee86caf0e1af378d70a
Steve Troetti <workstroetti@gmail.com>
2016-06-14 19:37:24 -0400

    Fixed db issues (hopefully)

--- server.js
+++ server.js
@@ -13,10 +13,7 @@ if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
         process.env.OPENSHIFT_APP_NAME;
-        // 'cs4550summer1';
 }
-
-console.log('using mongo connection string: ' + connectionString);
 
 var mongoose = require("mongoose");
 mongoose.connect(connectionString);
