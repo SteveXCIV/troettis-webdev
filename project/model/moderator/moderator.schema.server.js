e3bf0a1afb039af431a73d9ffa95fb222d900909
Steve Troetti <workstroetti@gmail.com>
2016-06-19 15:24:06 -0400

    Added moderator schema

---
+++ project/model/moderator/moderator.schema.server.js
@@ -0,0 +1,10 @@
+module.exports = function () {
+    var mongoose = require('mongoose');
+
+    var ModeratorSchema = mongoose.Schema({
+        community: { type: mongoose.Schema.Types.ObjectId, ref: 'Community', required: true },
+        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    });
+
+    return ModeratorSchema;
+};
