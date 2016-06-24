0a9cf85ace704752c15e01448e0e99071fa2d798
Steve Troetti <workstroetti@gmail.com>
2016-06-23 22:48:01 -0400

    Move logger into it's own file

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var log = require('./lib/logger')();
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -30,41 +31,6 @@ if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
 var mongoose = require("mongoose");
 mongoose.connect(connectionString);
 
-var shortid = require('shortid');
-// Attach a global ID generating function to app
-app.getNextId = function() {
-    return shortid.generate();
-}
-
-var _log_prefixes = {
-    ERROR: 'ERR',
-    WARNING: 'WARN',
-    INFO: 'INFO',
-    DEBUG: 'DBG',
-};
-
-function _log(level, message) {
-    if (!process.env.OPENSHIFT_APP_NAME) {
-        console.log(`[${_log_prefixes[level]}]:: ${message}`);
-    }
-}
-
-app.error = function(message, error) {
-    _log('ERROR', `${message} Error: ${JSON.stringify(error)}`);
-};
-
-app.warning = function(message) {
-    _log('WARNING', message);
-};
-
-app.info = function(message) {
-    _log('INFO', message);
-};
-
-app.debug = function(message) {
-    _log('DEBUG', message);
-}
-
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
@@ -75,5 +41,5 @@ require('./project/app.js')(app);
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-app.debug(`Server start at address (${ipaddress}) and port (${port}).`);
+log.debug(`Server start. IP: ${ipaddress || 'localhost'}, Port: ${port}`);
 app.listen(port, ipaddress);
