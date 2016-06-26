3b4c2a218dcf97167fc884ad9e87059398ba72fb
Steve Troetti <workstroetti@gmail.com>
2016-06-26 13:51:31 -0400

    Added public views

---
+++ public/project/config.js
@@ -0,0 +1,107 @@
+(function() {
+    angular
+        .module('Project')
+        .config(Config);
+
+    function Config($routeProvider) {
+        var checkLoggedIn = function (
+            $q,
+            $timeout,
+            $http,
+            $location,
+            $rootScope) {
+            var deferred = $q.defer();
+            $http
+                .get('/api/loggedin')
+                .success(
+                    function (user) {
+                        $rootScope.errorMessage = null;
+                        if (user !== '0') {
+                            $rootScope.currentUser = user;
+                            deferred.resolve();
+                        } else {
+                            $rootScope.errorMessage = 'You need to be logged in to do that.';
+                            deferred.reject();
+                            $location.url('/');
+                        }
+                    });
+            return deferred.promise;
+        };
+
+        $routeProvider
+            .when('/login', {
+                templateUrl: 'views/user/login.view.client.html',
+                controller: 'LoginController',
+                controllerAs: 'model',
+            })
+            .when('/register', {
+                templateUrl: 'views/user/register.view.client.html',
+                controller: 'RegisterController',
+                controllerAs: 'model',
+            })
+            .when('/user/:username', {
+                templateUrl: 'views/user/profile.view.client.html',
+                controller: 'ProfileController',
+                controllerAs: 'model',
+            })
+            .when('/profile/edit', {
+                templateUrl: 'views/user/profile.view.edit.client.html',
+                controller: 'ProfileEditController',
+                controllerAs: 'model',
+                resolve: { loggedIn: checkLoggedIn },
+            })
+            .when('/user/:uid/website', {
+                templateUrl: 'views/website/website-list.view.client.html',
+                controller: 'WebsiteListController',
+                controllerAs: 'model',
+            })
+            .when('/user/:uid/website/new', {
+                templateUrl: 'views/website/website-new.view.client.html',
+                controller: 'NewWebsiteController',
+                controllerAs: 'model',
+            })
+            .when('/user/:uid/website/:wid', {
+                templateUrl: 'views/website/website-edit.view.client.html',
+                controller: 'EditWebsiteController',
+                controllerAs: 'model',
+            })
+            .when('/user/:uid/website/:wid/page', {
+                templateUrl: 'views/page/page-list.view.client.html',
+                controller: 'PageListController',
+                controllerAs: 'model',
+            })
+            .when('/user/:uid/website/:wid/page/new', {
+                templateUrl: 'views/page/page-new.view.client.html',
+                controller: 'NewPageController',
+                controllerAs: 'model',
+            })
+            .when('/user/:uid/website/:wid/page/:pid', {
+                templateUrl: 'views/page/page-edit.view.client.html',
+                controller: 'EditPageController',
+                controllerAs: 'model',
+            })
+            .when('/user/:uid/website/:wid/page/:pid/widget', {
+                templateUrl: 'views/widget/widget-list.view.client.html',
+                controller: 'WidgetListController',
+                controllerAs: 'model',
+            })
+            .when('/user/:uid/website/:wid/page/:pid/widget/new', {
+                templateUrl: 'views/widget/widget-chooser.view.client.html',
+                controller: 'NewWidgetController',
+                controllerAs: 'model',
+            })
+            .when('/user/:uid/website/:wid/page/:pid/widget/:wgid', {
+                templateUrl: 'views/widget/widget-edit.view.client.html',
+                controller: 'EditWidgetController',
+                controllerAs: 'model',
+            })
+            .when('/user/:uid/website/:wid/page/:pid/widget/:wgid/flickr', {
+                templateUrl: 'views/widget/widget-flickr-search.client.html',
+                controller: 'FlickrImageSearchController',
+                controllerAs: 'model',
+            })
+            .otherwise({
+                redirectTo: '/login'
+            });
+    }
+})();
---
+++ public/project/views/user/user.controller.js
@@ -0,0 +1,148 @@
+(function() {
+    angular
+        .module('Project')
+        .controller('LoginController', LoginController)
+        .controller('RegisterController', RegisterController)
+        .controller('ProfileController', ProfileController)
+        .controller('ProfileEditController', ProfileEditController);
+
+    function LoginController($location, $rootScope, UserService) {
+        var vm = this;
+        vm.login = login;
+
+        function login(element, user) {
+            if (element.$invalid) {
+                return;
+            }
+
+            vm.alert = null;
+
+            UserService
+                .login(user)
+                .then(
+                    function(response) {
+                        var user = response.data;
+                        $rootScope.currentUser = user;
+                        $location.url('/profile/edit');
+                    },
+                    function(error) {
+                        vm.alert = error.data;
+                    }
+                );
+        }
+    }
+
+    function RegisterController($location, $rootScope, UserService) {
+        var vm = this;
+        vm.register = register;
+
+        function register(element, user) {
+            if (element.$invalid) {
+                return;
+            }
+
+            vm.alert = null;
+
+            UserService
+                .register(user)
+                .then(
+                    function(response) {
+                        var user = response.data;
+                        $rootScope.currentUser = user;
+                        $location.url('/profile/edit');
+                    },
+                    function(error) {
+                        vm.alert = error.data;
+                    }
+                );
+        }
+    }
+
+    function ProfileController($routeParams, $window, UserService) {
+        var vm = this;
+        vm.username = $routeParams['username'];
+        vm.back = back;
+
+        function init() {
+            UserService
+                .findUserByUsername(vm.username)
+                .then(
+                    function (response) {
+                        vm.user = response.data;
+                    },
+                    function (error) {
+                        vm.alert = error.data;
+                    });
+        }
+        init();
+
+        function back() {
+            $window.history.back();
+        }
+    }
+
+    function ProfileEditController($rootScope, $routeParams, $location, UserService) {
+        var vm = this;
+        vm.logout = logout;
+        vm.updateUser = updateUser;
+        vm.deleteUser = deleteUser;
+
+        function init() {
+            UserService
+                .findCurrentUser()
+                .then(
+                    function(response) {
+                        vm.user = response.data;
+                    },
+                    function(error) {
+                        vm.alert = error.data;
+                    }
+                );
+        }
+        init();
+
+        function logout() {
+            UserService
+                .logout()
+                .then(
+                    function (response) {
+                        $rootScope.currentUser = null;
+                        $location.url('/');
+                    },
+                    function (error) {
+                        vm.alert = error.data;
+                    });
+        }
+
+        function updateUser(user) {
+            vm.success = null;
+            vm.alert = null;
+            UserService
+                .updateUser(vm.user._id, user)
+                .then(
+                    function(response) {
+                        if (response.status === 200) {
+                            vm.success = 'Profile updated!';
+                        }
+                    },
+                    function(error) {
+                        console.log(error);
+                        vm.alert = error.data;
+                    }
+                );
+        }
+
+        function deleteUser() {
+            UserService
+                .deleteUser(vm.user._id)
+                .then(
+                    function(response) {
+                        $location.url('/');
+                        vm.success = response.data;
+                    },
+                    function(error) {
+                        vm.alert = error.data;
+                    });
+        }
+    }
+})();
